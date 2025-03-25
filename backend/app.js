ae729f929598a8bece6b2faeed45698a78fbd4dc
manavv-13 <anshnigam034@gmail.com>
2025-03-25 14:39:23 +0530

    Fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin :[],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 // Email Sender Function
 const transporter = nodemailer.createTransport({
