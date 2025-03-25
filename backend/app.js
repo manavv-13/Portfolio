5f13ba6927341238618a74dfdd96bbfb7842ed4f
manavv-13 <anshnigam034@gmail.com>
2025-03-25 14:51:38 +0530

    Fixed for deployment

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin :[],
+    origin :["https://manav-nigam-portfolio.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
